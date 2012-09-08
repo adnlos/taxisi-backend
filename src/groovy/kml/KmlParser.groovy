bf163f1de52479410c242004210f74814592707c
isramv74 <iceman1974>
2012-09-08 14:46:41 -0500

    points detection

--- src/groovy/kml/KmlParser.groovy
+++ src/groovy/kml/KmlParser.groovy
@@ -8,53 +8,14 @@ package kml
 class KmlParser {
     static ns = new groovy.xml.Namespace("http://www.opengis.net/kml/2.2", 'ns')
     List parse( Object input ) {
-
-
-        def xml = new XmlSlurper().parse(input)
-        def x = xml.depthFirst().findAll { it.name() == 'Point' }
-
-        x.each {point ->
-            println "*Point: "+point
-        }
-
-        x = xml.depthFirst().findAll { it.name() == 'LineString' }
-
-        x.each {point ->
-            println "*LineString: "+point
-        }
-
         def points = []
 
-        def kml = new groovy.util.XmlParser().parse( input );
-        def coords = kml[ ns.Document ][ns.Folder][ ns.Placemark ][ ns.Point ][ns.coordinates]
+        def xml = new XmlSlurper().parse(input)
+        def coordinates = xml.depthFirst().findAll { it.name() == 'coordinates' }
 
-        //def placemarks = kml[ ns.Document ][ns.Folder][ ns.Placemark ]
-        //placemarks = kml[ ns.Document ][ns.Folder][ ns.Folder ][ ns.Placemark ]
-
-
-        coords.each {point ->
+        coordinates.each {point ->
             points << new Point( point.text() )
-            println "Point: "+point
-        }
-
-        coords = kml[ ns.Document ][ns.Folder][ ns.Placemark ][ ns.LineString ][ns.coordinates]
-
-        coords.each {point ->
-            points << new Point( point.text() )
-            println "LineString: "+point
-        }
-
-        coords = kml[ ns.Document ][ns.Folder][ ns.Folder ][ ns.Placemark ][ ns.Point ][ns.coordinates]
-        coords.each {point ->
-            points << new Point( point.text() )
-            println "Point: "+point
-        }
-
-        coords = kml[ ns.Document ][ns.Folder][ ns.Folder ][ ns.Placemark ][ ns.LineString ][ns.coordinates]
-
-        coords.each {point ->
-            points << new Point( point.text() )
-            println "LineString: "+point
+            println "*coordinates: "+point
         }
 
         return points;
